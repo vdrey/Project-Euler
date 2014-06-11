afa729260d59b64d04e081e67743bdb63985889a
vdrey <daniel7reynolds@gmail.com>
2014-06-11 17:07:47 -0400

    Sieve v2 edit
    possList generator added

--- Sieve of Eratosthenes v2.py
+++ Sieve of Eratosthenes v2.py
@@ -1,1 +1,21 @@
 # Sieve version 2
+
+# This generates a list of odd numbers < TARGET 
+
+print('Enter your target value. (Must be greater than 24)')
+TARGET = input()
+TARGET = int(TARGET)
+
+termR1 = 5
+possList = [2,3]
+
+while termR1 < TARGET:
+
+    a = termR1
+    termR1 = termR1 + 2
+    possList.append(a)
+
+print(possList)
+print()
+print('You must press enter to continue')
+input()
