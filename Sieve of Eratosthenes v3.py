80f05084fa6afbc38a8e6a17aac32e38750de505
vdrey <daniel7reynolds@gmail.com>
2014-06-28 22:01:36 -0400

    Changing sieve approch

--- Sieve of Eratosthenes v3.py
+++ Sieve of Eratosthenes v3.py
@@ -1,21 +1,3 @@
 # Sieve version 3
 
-# This generates a list of odd numbers < TARGET 
-
-print('Enter your target value. (Must be greater than 24)')
-TARGET = input()
-TARGET = int(TARGET)
-
-termR1 = 5
-possList = [2,3]
-
-while termR1 < TARGET:
-
-    possList.append(termR1)
-    termR1 = termR1 + 2
-    print(possList[len(possList)-1])
-    
-print(len(possList))
-print()
-print('You must press enter to continue')
-input()
+# This time, the list will be generated while checking primality and if it is a factor or not all at the same time
