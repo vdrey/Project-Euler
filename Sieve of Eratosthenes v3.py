b01ceec23b42b8f6d7592ffc641ff59a5e5fffca
vdrey <daniel7reynolds@gmail.com>
2014-06-28 22:12:47 -0400

    Edit Sieve v3

--- Sieve of Eratosthenes v3.py
+++ Sieve of Eratosthenes v3.py
@@ -3,15 +3,19 @@
 print('Enter your target value. (Must be greater than 20)')
 TARGET = input()
 TARGET = int(TARGET)
+possTerm = 3
 
 # Def section
 
 def factCheck(possTerm):
-    if Target/possTerm
+    if Target/possTerm == int(Target/possTerm):
+        return True
+    else:
+        return False
 
 # This time, the list will be generated while checking primality and if it is a factor or not all at the same time
 
-possTerm = 3
+
 primeFactors = []
 a = 0
 
@@ -20,4 +24,5 @@ while possTerm < Target:
     possTerm = possTerm + (2 * a)
     a = a + 1
 
-    if Tar
+    if factCheck(possTerm) == True:
+        
