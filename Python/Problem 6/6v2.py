a808379ca1c1b7d993c259742300ce68695e59bd
vdrey <daniel7reynolds@gmail.com>
2014-06-30 17:04:19 -0400

    6v2 edits

--- Python/Problem 6/6v2.py
+++ Python/Problem 6/6v2.py
@@ -1,1 +1,18 @@
 # The second try at problem 6
+
+n = 1
+numList = []
+sqList = []
+
+while n <= 1000:
+    numList.append(n)
+    n = n + 1
+
+n = 1
+
+while n <= 1000:
+    sqList.append(n ** 2)
+    n = n + 1
+
+sumNum = sum(numList)
+
