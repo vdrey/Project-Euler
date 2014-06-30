4ef0a752c745f1476d9a7cc48786691855aea0d6
vdrey <daniel7reynolds@gmail.com>
2014-06-30 17:08:07 -0400

    6v2 now gets correct answer
    First 100 natural numbers, not 1000.

--- Python/Problem 6/6v2.py
+++ Python/Problem 6/6v2.py
@@ -4,15 +4,19 @@ n = 1
 numList = []
 sqList = []
 
-while n <= 1000:
+while n <= 100:
     numList.append(n)
     n = n + 1
 
 n = 1
 
-while n <= 1000:
+while n <= 100:
     sqList.append(n ** 2)
     n = n + 1
 
 sumNum = sum(numList)
+sumNumSq = sumNum ** 2
 
+sumSq = sum(sqList)
+
+print(sumNumSq - sumSq)
