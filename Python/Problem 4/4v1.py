304be62f330cec307ade5285fd2176b05b728613
vdrey <daniel7reynolds@gmail.com>
2014-06-30 14:18:28 -0400

    Problem 4v1 edits
    It worked

--- Python/Problem 4/4v1.py
+++ Python/Problem 4/4v1.py
@@ -1,1 +1,29 @@
 #Problem 4 solution v1
+
+def reverse(num):
+  return int(str(num)[::-1])
+
+a = 100
+y = []
+
+while a < 1000:
+
+    b = 100
+
+    while b < 1000:
+
+        if a * b == reverse(a * b):
+            y.append(a*b)
+            b = b + 1
+
+        else:
+            b = b +1
+
+    a = a + 1
+
+
+y.sort()
+
+x = y[len(y) - 1]
+
+print(x)
