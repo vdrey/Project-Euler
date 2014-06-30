a1630301a25b715b4851362d3b9c8703fe3431c0
vdrey <daniel7reynolds@gmail.com>
2014-06-30 16:52:39 -0400

    Final edit 6v1
    Start on 6v2 because I miss read the problem.

--- Python/Problem 6/6v1.py
+++ Python/Problem 6/6v1.py
@@ -1,1 +1,16 @@
 # First try at problem 6
+
+numSet = []
+n = 1
+
+while n <= 1000:
+
+    numSet.append(n)
+    n = n + 1
+
+setSum = sum(numSet)
+setSumSq = setSum ** 2
+
+print(setSumSq - setSum)
+
+#Misread begin work on v2
