4d95b0ce942281ac076ec9e5541f21e1eadd445f
vdrey <daniel7reynolds@gmail.com>
2014-06-30 14:31:29 -0400

    5v1 edits

--- Python/Problem 5/5v1.py
+++ Python/Problem 5/5v1.py
@@ -1,1 +1,25 @@
 # First attempt at problem 5
+
+testNum = 2520
+
+def factorCheck(num):
+
+    factorList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    score = 0
+
+    for term in factorList:
+
+        if testNum % term == 0:
+            score = score + 1
+
+        else:
+            break
+
+    if score == 20:
+        return True
+
+    else:
+        return False
+
+
+#Now use the definition
