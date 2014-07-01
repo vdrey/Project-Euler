9cd47650a0e763b8225d8a7921d0daa828f308ef
vdrey <daniel7reynolds@gmail.com>
2014-07-01 11:17:12 -0400

    25v1 edits

--- Python/Problems 20-29/Problem 25/25v1.py
+++ Python/Problems 20-29/Problem 25/25v1.py
@@ -1,1 +1,27 @@
 # First attempt of problem 25
+
+#[1, 2, 3, 5,...]
+#[a, x, y, z,...]
+
+z = 5
+y = 3
+x = 2
+terms = [1,2,3,]
+insif = False
+
+while len(str(terms[len(terms)-1])) < 1000:
+
+    if insif == True: #to find nth term, use a len statement
+        break
+
+    else:
+        terms.append(z)
+        a = x
+        b = y
+        c = z
+        z = b + c
+        y = b + a
+        x = b
+        print(terms[len(terms)-1])
+
+print(terms[len(terms)-1])
