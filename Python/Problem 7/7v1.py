8c4b3830051a30cbaa99de92feae26e944ddc038
vdrey <daniel7reynolds@gmail.com>
2014-06-30 20:18:51 -0400

    More 7v1 edits

--- Python/Problem 7/7v1.py
+++ Python/Problem 7/7v1.py
@@ -8,15 +8,19 @@ primes = [2,3,5,7,11,13,17,19,23,29]
 
 while len(primes) < 10000:
 
+    print(len(primes))
+    currLength = len(primes)
+
     for term in primes:
 
         if trial % term == 0:
             trial = trial + 1
+            break
 
         elif primes.index(term) == (len(primes) - 1) and trial % term != 0:
             primes.append(trial)
             trial = trial + 1
-            print(len(primes))
+            
 
         else:
             insif = insif + 1
