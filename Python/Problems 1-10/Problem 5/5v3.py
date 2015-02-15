a3a94ac9756394e2b7465cfc806d5bc5ec714b05
vdrey <daniel7reynolds@gmail.com>
2015-02-15 23:19:19 +0000

    5v3.py created

---
+++ Python/Problems 1-10/Problem 5/5v3.py
@@ -0,0 +1,16 @@
+# Third attept project euler #5
+
+def divCheck(n):
+
+    for i in range(1,21):
+        if n%i != 0:
+            return False 
+
+    return True
+
+n = 2
+
+while not divCheck(n):
+    n += 2
+
+print(n)
