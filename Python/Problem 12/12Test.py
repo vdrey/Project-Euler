839ad21c7f875aa7aa928f2a1641863310d53b60
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-02-21 19:34:32 -0500

    Tested program to find the number of divisors

--- Python/Problem 12/12Test.py
+++ Python/Problem 12/12Test.py
@@ -10,3 +10,15 @@ def genTri(terms): # This works
     return vals
 
 # print(genTri(70)) 
+
+def getNumDivisors(num): # This works
+    divs = []
+    for i in range(1, num // 2):
+        if num % i == 0:
+            if i not in divs:
+                divs.append(i)
+            if num // i not in divs:
+                divs.append(num // i)
+    return len(divs)
+
+# print(getNumDivisors(2800))
