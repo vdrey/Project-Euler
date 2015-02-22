d9121ad189cf1aa59689b2d6b5b5abc48983687e
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-02-21 20:18:13 -0500

    12v1 is in progress

---
+++ Python/Problem 12/12v1.py
@@ -0,0 +1,31 @@
+# This is the first attempt at Proj. Euler number 12
+
+def getNumDivisors(num): # This works
+    divs = []
+    for i in range(1, num // 2):
+        if num % i == 0:
+            if i not in divs:
+                divs.append(i)
+            if num // i not in divs:
+                divs.append(num // i)
+    return len(divs)
+
+def nextTri(currentTerm, currentInt): # This works
+    n = currentInt + 1
+    nextTerm = currentTerm + n
+    return nextTerm
+
+term = 1493856
+counter = 1728
+while getNumDivisors(term) <= 210:
+    term = nextTri(term, counter)
+    counter += 1
+    
+
+print('counter: %s' % (counter))
+print()
+print()
+print('divisors: %s' % (getNumDivisors(term)))
+print()
+print()
+print(term)
